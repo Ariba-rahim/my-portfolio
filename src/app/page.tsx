76025e60876927bb4cbb273a3a8dbc06fa34f8af
Ariba-rahim <ariba.rahimkhan@gmail.com>
2025-05-28 20:06:32 +0500

    Your commit message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,37 +6,40 @@ import Image from 'next/image';
 
 export default function HomePage() {
   return (
+        
     <div
-    className="min-h-screen bg-cover bg-center bg-no-repeat flex flex-col items-center justify-center text-white "
+    className="min-h-screen bg-cover bg-center bg-no-repeat flex flex-col text-white h-screen"
     style={{ backgroundImage: "url('/bg.jpg')" }}
   >
-      <Navbar />
+    <Navbar />
   
-      <main className="flex flex-col items-center justify-center min-h-[80vh] text-center px-4 pt-20">
+      <main className="flex flex-col lg:flex-row items-start justify-start flex-grow pt-36 pl-40">
         <Image
-          src="/ii.jpg"
+          src="/pp.png"
           alt="Profile"
-          width={160}
-          height={160}
-          className="rounded-full mb-6 shadow-lg border-4 border-blue-200"
+          width={300}
+          height={400}
+          className="border-4 border-blue-500 shadow-lg"
         />
-
-        <h1 className="text-5xl sm:text-6xl font-extrabold text-white mb-4 leading-tight animate-fade-in bg-opacity-60 p-8 rounded-lg">
+<div className="text-white  pl-14 leading-relaxed max-w-4xl">
+        <h1 className="text-5xl font-extrabold leading-tight drop-shadow-black mb-6 text-shadow-black">
           Hi, I'm <span className="text-white">Ariba Rahim</span>
         </h1>
-        <p className="text-lg sm:text-xl text-white b-6 max-w-xl">
+        <p className=" leading-relaxed mb-6 text-white/90 text-3xl shadow-black max-w-4xl font-bold">
           I’m a full-stack developer who builds clean and modern web applications.
         </p>
         <a
           href="/projects"
-          className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-3 px-6 rounded-xl transition duration-300 shadow-md"
+          className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-4 px-6 rounded-xl transition duration-300 shadow-md justify-center"
         >
           View My Projects
         </a>
+        </div>
       </main>
-
-      <Footer/>
+  <Footer/>
       </div>
+    
+      
   );
 }
 
